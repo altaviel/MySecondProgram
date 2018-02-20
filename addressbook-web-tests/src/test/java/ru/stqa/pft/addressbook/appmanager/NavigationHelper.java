69a18d1dcc83fad2e4b0e157853e6855fcc53863
altaviel <36173997+altaviel@users.noreply.github.com>
2018-02-20 19:43:32 +0300

    Добавлен общий базовый класс и низкоуровневые вспомогательные методы.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,20 +3,17 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NavigationHelper {
-    private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void goToGroupPage() {
-        wd.findElement(By.id("header")).click();
-        wd.findElement(By.linkText("groups")).click();
+        click (By.linkText("groups"));
     }
 
     public void returnToMainPage() {
-        wd.findElement(By.id("header")).click();
-        wd.findElement(By.linkText("home")).click();
+       click(By.linkText("home"));
     }
 }
